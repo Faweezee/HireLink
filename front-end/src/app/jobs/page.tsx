6015e32c8cc081580679cf5299a129526798f0d9
Fawaz Salimanu <salimanufawaz@gmail.com>
2026-05-02 21:29:23 +0100

    feat: re-initiated Jobs page

--- front-end/src/app/jobs/page.tsx
+++ front-end/src/app/jobs/page.tsx
@@ -1,10 +1,5 @@
-"use client";
+const JobsPage = () => {
+  return <div></div>;
+};
 
-export default function JobsPage() {
-  return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">Jobs</h1>
-      <p>This page is under construction.</p>
-    </div>
-  );
-}
+export default JobsPage;
